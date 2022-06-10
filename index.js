3119355bb79b80bfaea5248a69f90e3663b39fec
Ezra-code <njerukithinji872@gmail.com>
2022-06-10 13:34:44 +0300

    commit tree 06a85c87b658f2236a3c475066fa732acae3b299

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arrayOfDrivers, name){
+    return arrayOfDrivers.filter((item) => 
+        item.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+}
+
+function fuzzyMatch(listOfDrivers, character){
+    return listOfDrivers.filter((driver) => driver.toLowerCase().indexOf(character.toLowerCase()) === 0
+    )
+}
+
+function matchName(listOfDrivers, name){
+    return listOfDrivers.filter((driver) => driver.name === name
+    )
+
+}
+
